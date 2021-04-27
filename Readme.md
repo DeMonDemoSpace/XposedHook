b2e09b5dd110b5be257b780cb141c213c961817a
DeMon <757454343@qq.com>
2021-04-27 09:48:29 +0800

    first

---
+++ Readme.md
@@ -0,0 +1,10 @@
+### Xposed-Hook
+
+使用Xposed可以反射检测目标App调用了哪些方法。
+
+参考:<https://www.jianshu.com/p/84127032d15a>
+
+根目录apk文件夹下：
+
+1. VirtualXposed_0.18.2.apk 支持32位应用的版本
+2. VirtualXposed_0.20.3.apk 只支持64位应用的版本
---
+++ app/src/main/java/com/demon/xposed_hook/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.demon.xposed_hook
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
